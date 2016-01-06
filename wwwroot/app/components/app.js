abec4c98b0c45d2ac86eefd5f92bc85bd1e1536d
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2016-01-06 18:18:11 -0500

    delete generated files

--- wwwroot/app/components/app.js
+++
@@ -1,8 +0,0 @@
-var AppComponent = (function () {
-    function AppComponent() {
-    }
-    AppComponent.message = "For The Glory!";
-    return AppComponent;
-})();
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.js.map
