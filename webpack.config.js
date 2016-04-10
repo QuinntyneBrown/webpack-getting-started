96b7ef55bb280d6feebb154268998af0233e0e85
Quinntyne Brown <quinntynebrown@gmail.com>
2016-04-10 13:46:52 -0400

    multiple modules

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+﻿module.exports = {
+    entry: {
+        "main": "./main.ts",
+        "main-other": "./main-other.ts",
+    },
+    output: {
+        path:"/dist",
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web,js', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/, loader: "ts-loader"
+            },
+            {
+                test: /\.css$/, exclude: [/node_modules/], loader: "style-loader!css-loader"
+            }
+        ]
+    }
+
+}
